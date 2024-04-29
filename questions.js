30dc6731da03bd1022847817471dd20acc0fe555
NalamSrinikitha <102012595+NalamSrinikitha@users.noreply.github.com>
2024-04-29 18:39:27 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Grand Central Terminal, Park Avenue, New York is the world's",
+    answer: "Largest railway station",
+    options: [
+      "Largest railway station",
+      "highest railway station",
+      "longest railway station",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Garampani sanctuary is located at",
+    answer: "Diphu, Assam",
+    options: [
+      "Junagarh, Gujarat",
+      "Diphu, Assam",
+      "Kohima, Nagaland",
+      "Gangtok, Sikkim"
+    ]
+  },
+  {
+    numb: 7,
+    question: "'OS' computer abbreviation usually means ?",
+    answer: "Operating System",
+    options: [
+      "Order of Significance",
+      "Open Software",
+      "Operating System",
+      "Optical Sensor"
+    ]
+  },
+  {
+    numb: 8,
+    question: "'.MOV' extension refers usually to what kind of file?",
+    answer: "Animation/movie file",
+    options: [
+      "Image file",
+      "Animation/movie file",
+      "Audio file",
+      "MS Office document"
+    ]
+  },
+  {
+    numb: 9,
+    question: " Which was the 1st non Test playing country to beat India in an international match?",
+    answer: "Sri Lanka",
+    options: [
+      "Canada",
+      "Sri Lanka",
+      "Zimbabwe",
+      "East Africa"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The name of the Laccadive, Minicoy and Amindivi islands was changed to Lakshadweep by an Act of Parliament in",
+    answer: "1973",
+    options: [
+      "1970",
+      "1971",
+      "1972",
+      "1973"
+    ]
+  },
+
+];
